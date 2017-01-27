524a1be0ecaf92cbcc16be630b6e473983a5bb40
Joe Sauve <joe.sauve@xamarin.com>
2017-01-26 17:06:39 -0800

    Initial commit

---
+++ Clicky/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Clicky
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int _NumberOfClicks;
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _NumberOfClicks++;
+            MyLabel.Text = String.Format("Clicked {0} times!", _NumberOfClicks);
+        }
+    }
+}
